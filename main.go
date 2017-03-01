e9490e35236c38eef40ee1ec2502fac7eebbde5b
Panziera, Luca <luca.panziera@ft.com>
2017-03-01 17:42:17 +0000

    Added main function and vendoring configuration

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"os"
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+	cli "gopkg.in/urfave/cli.v1"
+)
+
+func init() {
+	f := &log.TextFormatter{
+		FullTimestamp:   true,
+		TimestampFormat: time.RFC3339Nano,
+	}
+
+	log.SetFormatter(f)
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "publish-carousel"
+	app.Usage = "A microservice that continuously republish content and annotations available in native store."
+	app.Action = func() {
+		log.Info("Hello World!")
+	}
+
+	app.Run(os.Args)
+}
