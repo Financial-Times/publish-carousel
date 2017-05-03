27793b0d2ab190d2529afb957db9e24d357ac9a0
Clark, Peter <peter.clark@ft.com>
2017-05-03 14:17:15 +0100

    :sparkles: Parsing swagger yml, and replacing the host field with the host of the request

--- resources/api.go
+++ resources/api.go
@@ -1,6 +1,11 @@
 package resources
 
-import "net/http"
+import (
+	"net/http"
+	"net/url"
+
+	yaml "gopkg.in/yaml.v2"
+)
 
 // API returns the swagger.yml for this service.
 func API(api []byte) func(w http.ResponseWriter, r *http.Request) {
@@ -10,7 +15,28 @@ func API(api []byte) func(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
+		swagger := make(map[string]interface{})
+		err := yaml.Unmarshal(api, swagger)
+		if err != nil {
+			outputStaticAPI(w, api)
+			return
+		}
+
+		uri, _ := url.Parse(r.RequestURI) // must be a valid url
+
+		swagger["host"] = uri.Host
+		updatedAPI, err := yaml.Marshal(swagger)
+		if err != nil {
+			outputStaticAPI(w, api)
+			return
+		}
+
 		w.Header().Add("Content-Type", "text/vnd.yaml")
-		w.Write(api)
+		w.Write(updatedAPI)
 	}
+}
+
+func outputStaticAPI(w http.ResponseWriter, api []byte) {
+	w.Header().Add("Content-Type", "text/vnd.yaml")
+	w.Write(api)
 }
